aa73c8c0d2a795fb83fe64829e714b60e2c707a0
Daniel <darkshoxx@yahoo.de>
2021-02-12 13:09:15 +0100

    Ex7

---
+++ 21_02_12_ex7.py
@@ -0,0 +1,22 @@
+print("Mary had a little lamb.")  # Printing a string
+print("Its fleece was white as {}.".format('snow'))  # Making a string, formatting it, then printing
+print("And everywhere that Mary went.")  # Printing a string
+print("." * 10)  # what'd that do?   # making 10 copies of that string in sequence in a string, then printing it
+
+end1 = "C"  # Saving a letter as a string in a variable
+end2 = "h"  # Saving a letter as a string in a variable
+end3 = "e"  # Saving a letter as a string in a variable
+end4 = "e"  # Saving a letter as a string in a variable
+end5 = "s"  # Saving a letter as a string in a variable
+end6 = "e"  # Saving a letter as a string in a variable
+end7 = "B"  # Saving a letter as a string in a variable
+end8 = "u"  # Saving a letter as a string in a variable
+end9 = "r"  # Saving a letter as a string in a variable
+end10 = "g"  # Saving a letter as a string in a variable
+end11 = "e"  # Saving a letter as a string in a variable
+end12 = "r"  # Saving a letter as a string in a variable
+
+# watch that comma at the end. try removing it to see what happens  # Nothing, it's a comment
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')  # Concatenating the first six strings,
+# printing them, and a whitespace in the end.
+print(end7 + end8 + end9 + end10 + end11 + end12)  # Printing the final strings, concatenated.
