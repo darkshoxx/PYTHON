e1bf4e183d5b24a7c6c714c7fc2cc5805422433c
Daniel <darkshoxx@yahoo.de>
2021-02-15 12:49:30 +0100

    Ex 22

---
+++ Exercises/21_02_15_ex22.py
@@ -0,0 +1,55 @@
+# # # Octothorpe, Comments out code.
+#   # Whitespace/space, to be put between 2 different objects for separation and readability
+#      # Indent, starting a new block of code depending on the previous non-indented line
+# print # print, Prints the string in it's parentheses.
+# " # Double Quote, start or end of string.
+# ' # Single Quote, start or end of string.
+# ( # opened parentheses, denotes the start of what parameters to be passed on.
+# ) # closed parentheses, denotes the end of what parameters to be passed on.
+# , # comma, separates individual arguments within a function call.
+# +-**% # (...), elementary math operations, binary functions.
+# - # negative, reverses the sign of the number/variable after it.
+# >< <= >= # (...) Comparing the size of two values. Returns boolean.
+# = # equals, assigns the value on its right to the variable on its left, or unpacks.
+# . # point, converts integer to float.
+# _ # underscore, connector for multiple words in a file- or variable name.
+# f # f, for formatted strings at the beginning of a string.
+# { # open curly brackets, in formatted strings, 'escapes' the string, whatever is in those is code, not string.
+# } # closed curly brackets, see above.
+# round # round, rounds value to predetermined number of digits.
+# True, False # (...) Boolean values.
+# .format() # converts string to formatted string.
+# + # plus, concatenates two strings.
+# * # times, makes integer number of copies of the string.
+# end # end, parameter of print, replaces the linebreak at the end of the printout with its parameter.
+# \ # backslash, escaping the preceding from the string.
+# \n # newline, new line.
+# """ # triple quotes, a string that can run across multiple lines.
+# \t # tab, creates tab/indent escaped string.
+# \\ # backslash, escaped backslash in string.
+# input # input, takes input from the user. Include argument for prompt on the command line.
+# from # from, loads object/function/variable/class from library/module or other.
+# sys # sys, module of system specific parameters and functions.
+# import # import, belongs to from.
+# argv # argv, put all the objects of the most recent line call in a list called argv.
+# open # open, opens a file, possibly wih modes.
+# .read # read, gives the contents of the file as a string.
+# .close # close, closes an opened file, and frees up the allocated memory.
+# .truncate # truncate, removes all bytes of pointer position + n. Default is current position.
+# .write # write, (over)writes the specified string into the file.
+# r # read mode, open file for reading, first default.
+# w # write mode, will truncate file first.
+# x # x, creates new file to write into, and opens it.
+# a # append mode, opens file for writing to the end of the file.
+# b # binary mode, opens file as binary.
+# t # text mode, opens file as text, second default.
+# + # plus, reading and writing.
+# os # os, module of miscellaneous operating system interfaces.
+# os.path # path, common pathname manipulations.
+# len # length, gives number of bytes of a file or number of objects in a list.
+# exists # exists, returns boolean of whether a file already exists or not.
+# * # asterisk, takes all given inputs to function, puts them into a tuple.
+# def # def, indicates the start of the definition of a function.
+# : # colon, at end of certain structures, followed by a line break and an indent.
+# readline # readline, reads everything from the pointer to the next linebreak.
+# += # plus-equal/increment, increments the object on the left by the number on the right.
