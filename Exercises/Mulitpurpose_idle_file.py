a008e54afcce47c5e79da131d9a918fe18515648
Daniel <darkshoxx@yahoo.de>
2021-02-21 14:03:13 +0100

    Ex 41

---
+++ Exercises/Mulitpurpose_idle_file.py
@@ -0,0 +1,16 @@
+w = "2"
+y = "4"
+z = "dlfgh oeghi er gouidsfhoigp st"
+LIIIST = [w, y, z]
+for blah in LIIIST:
+    print(blah[:])
+    print(blah)
+
+
+def square(x):
+    return pow(x, 2)
+
+
+a = 14
+square(a)
+
