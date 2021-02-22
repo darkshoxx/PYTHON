6f950f89861d56dd13da94b4d898d7f4ca2ce71f
Daniel <darkshoxx@yahoo.de>
2021-02-22 13:37:10 +0100

    Ex 42 and multi

--- Exercises/Mulitpurpose_idle_file.py
+++ Exercises/Mulitpurpose_idle_file.py
@@ -1,16 +1,40 @@
-w = "2"
-y = "4"
-z = "dlfgh oeghi er gouidsfhoigp st"
-LIIIST = [w, y, z]
-for blah in LIIIST:
-    print(blah[:])
-    print(blah)
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * self.length + 2 * self.width
 
 
-def square(x):
-    return pow(x, 2)
+class Square(Rectangle):
+    def __init__(self, length):
+        super(Square, self).__init__(length, length)
 
 
-a = 14
-square(a)
+class Triangle:
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
 
+    def area(self):
+        return 0.5 * self.base * self.height
+
+
+class RightPyramid(Square, Triangle):
+    def __init__(self, base, slant_height):
+        self.base = base
+        self.slant_height = slant_height
+        super().__init__(self.base)
+
+    def area(self):
+        base_area = super().area()
+        perimeter = super().perimeter()
+        return 0.5 * perimeter * self.slant_height + base_area
+
+
+pyr = RightPyramid(1, 1)
+print(pyr.area())
